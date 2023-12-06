6ef2f8e2df143353c408603a910ae8cffd8216ef
Gabriel Mendes <85296475+arvlotus@users.noreply.github.com>
2023-12-06 04:20:06 -0300

    Estilizando index

--- index.php
+++ index.php
@@ -2,10 +2,17 @@
 include_once(__DIR__ . '/components/header.php');
 ?>
 
-<main class="container">
-  <h1 class="text-center mt-3 mb-5">Pagina Inicial</h1>
-</main>
 
+<section class="iniciofundo">
+<main class="container">
+    <h1 class="text-center mt-3 mb-1">Pagina Inicial</h1>
+    <small class="form-text text-muted ">
+      <h4 class="text-center mb-4">Cursos mais acessados no momento:</h4>
+    </small>
+  </main>
+</section>
+
+<section class="carouselfundo">
 <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
@@ -17,8 +24,15 @@ include_once(__DIR__ . '/components/header.php');
     <div class="carousel-item">
       <img src="css/Infoway/PHP POO.png" class="d-block w-50 mx-auto" alt="imagen3">
     </div>
+    <div class="carousel-item">
+      <img src="css/Infoway/HTMLCSSJAVA.jpeg" class="d-block w-50 mx-auto" alt="imagen3">
+    </div>
+    <div class="carousel-item">
+      <img src="css/Infoway/INFORMATICABASICA.jpeg" class="d-block w-50 mx-auto" alt="imagen3">
+    </div>
   </div>
 </div>
+</section>
 
 <?php
 include_once(__DIR__ . '/components/footer.php');
