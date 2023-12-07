21e0cc6f37ac0b88471b8ecf1707344beda32330
Gabriel Mendes <85296475+arvlotus@users.noreply.github.com>
2023-12-06 21:37:35 -0300

    Mudando cor do texto footer

--- index.php
+++ index.php
@@ -4,41 +4,46 @@ include_once(__DIR__ . '/components/header.php');
 
 
 <section class="iniciofundo">
-<main class="container">
-<div class="textoinicial">
-<p>Bem-vindo ao nosso universo digital onde a inovação encontra a informação!<br> No coração do ciberespaço,
-          é com grande entusiasmo que recebemos você<br> em nosso site de informática, um espaço dedicado à descoberta
-          e ao aprimoramento constante.<br> Prepare-se para uma imersão no universo fascinante da informática,<br>
-          onde a cada clique uma nova porta se abre para possibilidades infinitas.<br> Estamos empolgados em tê-lo conosco nesta jornada eletrônica.</p>
+
+  <div class="imagemdoinicio ml-auto">
+  <img src="" alt="">
+  </div>
+
+  <main class="container">
+    <div class="textoinicial">
+      <p>Bem-vindo ao nosso universo digital onde a inovação encontra a informação!<br> No coração do ciberespaço,
+        é com grande entusiasmo que recebemos você<br> em nosso site de informática, um espaço dedicado à descoberta
+        e ao aprimoramento constante.<br> Prepare-se para uma imersão no universo fascinante da informática,<br>
+        onde a cada clique uma nova porta se abre para possibilidades infinitas.<br> Estamos empolgados em tê-lo conosco nesta jornada eletrônica.</p>
       <form class="form-inline my-2 my-lg-0">
         <a class="btn btn-custom" href="register.php">
           Junte-se a nós
         </a>
-</div>
-</main>
+    </div>
+  </main>
 </section>
 
 
 <section class="carouselfundo">
-<div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-  <div class="carousel-inner">
-    <div class="carousel-item active">
-      <img src="css/Infoway/HARDWARE.png" class="d-block w-50 mx-auto" alt="imagen1">
-    </div>
-    <div class="carousel-item">
-      <img src="css/Infoway/JAVASCRIPT.png" class="d-block w-50 mx-auto" alt="imagen2">
-    </div>
-    <div class="carousel-item">
-      <img src="css/Infoway/PHP POO.png" class="d-block w-50 mx-auto" alt="imagen3">
-    </div>
-    <div class="carousel-item">
-      <img src="css/Infoway/HTMLCSSJAVA.jpeg" class="d-block w-50 mx-auto" alt="imagen3">
-    </div>
-    <div class="carousel-item">
-      <img src="css/Infoway/INFORMATICABASICA.jpeg" class="d-block w-50 mx-auto" alt="imagen3">
+  <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src="css/Infoway/HARDWARE.png" class="d-block w-50 mx-auto" alt="imagen1">
+      </div>
+      <div class="carousel-item">
+        <img src="css/Infoway/JAVASCRIPT.png" class="d-block w-50 mx-auto" alt="imagen2">
+      </div>
+      <div class="carousel-item">
+        <img src="css/Infoway/PHP POO.png" class="d-block w-50 mx-auto" alt="imagen3">
+      </div>
+      <div class="carousel-item">
+        <img src="css/Infoway/HTMLCSSJAVA.jpeg" class="d-block w-50 mx-auto" alt="imagen3">
+      </div>
+      <div class="carousel-item">
+        <img src="css/Infoway/INFORMATICABASICA.jpeg" class="d-block w-50 mx-auto" alt="imagen3">
+      </div>
     </div>
   </div>
-</div>
 </section>
 
 <?php
