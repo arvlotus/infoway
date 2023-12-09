2e90de93f4da0e3cb88340f2b4274002c8fd4167
Gabriel Mendes <85296475+arvlotus@users.noreply.github.com>
2023-12-09 15:18:12 -0300

    Estilizando o sobre

--- components/footer.php
+++ components/footer.php
@@ -1,7 +1,9 @@
 <footer class="mt-5 mb-2">
+    <div class="footer">
     <p class="text-center">
         Infoway. Todos os direitos reservados
     </p>
+    </div>
 </footer>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
--- sobre.php
+++ sobre.php
@@ -1,19 +1,52 @@
 <?php
 $currentPage = 'sobre';
-include_once(__DIR__.'/components/header.php');
+include_once(__DIR__ . '/components/header.php');
 ?>
 
 <!-- Sobre -->
 
 <main class="containersobre">
-  <div class="textsobre">
-  <h1 class="text-center mt-3">Sobre</h1>
-  <p>Bem-vindo à Infoway, sua plataforma educacional especializada em proporcionar uma experiência de aprendizado excepcional em tecnologia da informação.<br>
-    Explore nosso catálogo diversificado de cursos e mergulhe em conhecimentos essenciais para impulsionar sua carreira no mundo digital.</p>
+  <div class="textsobre mb-5">
+    <h1 class="text-center mt-3">
+      Sobre
+    </h1>
+    <div class="paragrafos">
+      <p class="text-center">Somos uma plataforma focada em proporcionar uma experiência de aprendizado excepcional
+        em tecnologia da informação.<br>
+        Explore nosso catálogo diversificado de cursos e mergulhe em
+        conhecimentos essenciais para impulsionar sua carreira no mundo digital.</p>
+      <p class="text-center">Em um cenário de constante evolução tecnológica, nossos cursos surgem
+        como uma resposta dinâmica às demandas crescentes por profissionais capacitados na área da tecnologia.
+        Com um foco abrangente
+        que engloba desde hardware até desenvolvimento de sistemas, lógica de programação e linguagens,<br>
+        nosso programa educacional visa preparar indivíduos para os desafios do mundo digital.</p>
+    </div>
   </div>
+
 </main>
 
+<main2>
+  <div class="textsobre2 mt-3">
+    <h1 class="text-center mt-3">
+      Equipe
+    </h1>
+  </div>
+  <div class="paragrafos2">
+  <p class="text-center mb-4">
+    Central para o sucesso de nosso projeto é uma equipe dedicada e especializada.
+  Contamos com profissionais experientes em hardware, desenvolvimento de sistemas,
+  e especialistas em diversas linguagens de programação. Nossa equipe é apaixonada
+  por compartilhar conhecimento e orientar os alunos em sua jornada de aprendizado.
+  </p>
+  </div>
+    <section class="image">
+      <div class="imgequipe justify-center">
+            <img src="css/Infoway/HTMLCSSJAVA.jpeg" alt="imgequipe" class="w-50">
+          </div>
+    </section>
+</div>
+</main2>
 
 <?php
-include_once(__DIR__.'/components/footer.php');
+include_once(__DIR__ . '/components/footer.php');
 ?>
