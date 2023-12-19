966d3e58e3427c726af50aec8e01a4a59c456f39
arvlotus <gabrmendes01@gmail.com>
2023-12-19 16:57:02 -0300

    Estilizando index, registro e login

---
+++ components/footer.php
@@ -0,0 +1,10 @@
+<footer class="mt-5 mb-2">
+    <p class="text-center">
+        Infoway. Todos os direitos reservados
+    </p>
+</footer>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+
+</body>
+</html>
--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
 
 $pageInfo = array(
- 'title' => 'Pagina inicial - Inforway',
- 'description' => 'Seja bem-vindo(a) ao Inforway.',
- 'pageName' => 'index',
+  'title' => 'Pagina inicial - Inforway',
+  'description' => 'Seja bem-vindo(a) ao Inforway.',
+  'pageName' => 'index',
 );
 
 $pageName = $pageInfo['pageName'];
@@ -35,25 +35,63 @@ include_once(__DIR__ . '/components/public/header.php');
   </div>
 </div>
 
+<div class="row">
+  <div class="card-cursos col-md-4 mb-5">
+    <h4>
+      HTML5, CSS & JAVASCRIPT
+    </h4>
+    <div class="cursoimg">
+      <img class="curso-img mb-2" src="css/Inforway/ICONS/HTML5.png" alt="informaticabasica">
+    </div>
+    <p>
+      Você irá aprender HTML, CSS e JavaScript e ultilizar para criar sites e aplicativos da Web
+    </p>
+  </div>
+  <div class="card-cursos col-md-4 mb-5">
+    <h4>
+      Logica de programação
+    </h4>
+    <div class="cursoimg">
+      <img class="curso-img mb-2" src="css/Inforway/ICONS/LÓGICA DE PROGRAMAÇÃO.png" alt="informaticabasica">
+    </div>
+    <p>
+    Neste curso, vamos apresentar conceitos de programação para você iniciar sua vida profissional.
+    </p>
+  </div>
+  <div class="card-cursos col-md-4 mb-5">
+    <h4>
+      Informática básica
+    </h4>
+    <div class="cursoimg">
+      <img class="curso-img mb-2" src="css/Inforway/ICONS/INFORMÁTICA BÁSICA.png" alt="informaticabasica">
+    </div>
+    <p>
+      A Informática básica te possibilita desenpenhar diferentes tarefas em diversos cargos.
+    </p>
+  </div>
+</div>
+
 <div class="ver-mais mb-5">
-  <a href="cursos.php" style=" width :400px; " class="btn btn-index"><h1>Ver mais cursos</h1></a>
+  <a href="cursos.php" style=" width :400px; " class="btn btn-index">
+    <h1>Ver mais cursos</h1>
+  </a>
 </div>
 
 
 <main class="container-index">
-    
-    <div class="textinicio">
-      <p>Bem-vindo ao nosso universo digital onde a inovação encontra a informação!<br> No coração do ciberespaço,
+
+  <div class="textinicio">
+    <p>Bem-vindo ao nosso universo digital onde a inovação encontra a informação!<br> No coração do ciberespaço,
       é com grande entusiasmo que recebemos você<br> em nosso site de informática, um espaço dedicado à descoberta
-        e ao aprimoramento constante.<br> Prepare-se para uma imersão no universo fascinante da informática,<br>
-        onde a cada clique uma nova porta se abre para possibilidades infinitas.<br> Estamos empolgados em tê-lo 
-        conosco nesta jornada eletrônica.</p>
-        <form class="form-inline my-2 my-lg-0">
-          <a class="btn btn-custom mt-5 mb-3" href="register.php">
-            Junte-se a nós
-          </a>
-        </form>
-    </div>
+      e ao aprimoramento constante.<br> Prepare-se para uma imersão no universo fascinante da informática,<br>
+      onde a cada clique uma nova porta se abre para possibilidades infinitas.<br> Estamos empolgados em tê-lo
+      conosco nesta jornada eletrônica.</p>
+    <form class="form-inline my-2 my-lg-0">
+      <a class="btn btn-custom mt-5 mb-3" href="register.php">
+        Junte-se a nós
+      </a>
+    </form>
+  </div>
 
   <div class="imageinicio p-3">
     <img src="css/Inforway/INFORWAY.png" alt="logo">
